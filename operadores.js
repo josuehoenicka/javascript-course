d2581d83ba09cd7718de1b21db4c913d9124f62a
Josué Hoenicka <josuee.vzla@gmail.com>
2023-01-24 12:30:39 -0300

    new: operadores

---
+++ operadores.js
@@ -0,0 +1,38 @@
+//Operadores binarios
+3 + 2 // 5
+50 - 10 // 40
+10 * 20 // 200
+20 / 2 // 10
+
+// Concatenación
+"Josue " + "Hoenicka" // Josue Hoenicka
+
+//Operadores unitarios
+!true // false
+!false // true
+
+// Operadores de asignación
+var a = 1; // 1
+
+// Operadores para comparar valor
+3 == "3"; // true
+
+// Operadores para comparar tipo y valor
+3 === "3"; // false
+
+
+// Operadores mayor, menos o igual qué
+5 < 3 // false
+5 > 3 // true
+5 <= 6 // true
+5 >= 6 // false
+
+// Condiciones
+a && b // false
+a || b // true
+
+// Aumentar o disminuir (opción 1)
+numeroUno++ // 2
+
+// Aumentar o disminuir (opción 1)
+numeroUno += 9 // 10 
