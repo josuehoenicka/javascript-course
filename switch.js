f78fd2be4e18e3a08afa21484c083d979c9b6174
Josué Hoenicka <josuee.vzla@gmail.com>
2023-01-25 08:18:19 -0300

    update: finish exercise

---
+++ switch.js
@@ -0,0 +1,15 @@
+let numero = 10;
+
+switch (numero) {
+    case 1: 
+        console.log("numero es: " + 1);
+        break;
+    case 10: 
+        console.log("numero es: " + 10);
+        break;
+    case 100: 
+        console.log("numero es: " + 100);
+        break;
+}
+
+// 10
